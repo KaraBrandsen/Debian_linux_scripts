e4fa5b58eceae40792582bfd0252917dab13792b
Kara Brandsen <karabrandsen@gmail.com>
2024-11-08 11:31:48 +0100

    Updated folder Owner ship

--- install/individual/install_wol_server.sh
+++ install/individual/install_wol_server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-LIBREST_PORT=8079                                       #Port used by Wake on LAN server
+WOL_SERVER_PORT=8079                                       #Port used by Wake on LAN server
+DEST_MAC=02:00:d3:f0:b1:4d                                 #MAC Address of the machine to wake up
+DEST_IP=192.168.194.1                                      #IP Address of the machine to wake up
+DEST_PORT=9                                                #Target port. Port 9 is used for wake on lan
+DEST_SUBNET=24                                             #Target Subnet.
 
 
 echo "-----------------------------Installing Wake on LAN Server-----------------------------"
@@ -12,7 +16,7 @@ rm -rf /etc/nginx/sites-available/wol-server
 
 echo "Configuring Nginx"
 echo "server {" > /etc/nginx/sites-available/wol-server
-echo "    listen $LIBREST_PORT;" >> /etc/nginx/sites-available/wol-server
+echo "    listen $WOL_SERVER_PORT;" >> /etc/nginx/sites-available/wol-server
 echo "    server_name wol-server www.wol-server;" >> /etc/nginx/sites-available/wol-server
 echo "    root /var/www/html/wol-server;" >> /etc/nginx/sites-available/wol-server
 echo "" >> /etc/nginx/sites-available/wol-server
@@ -48,7 +52,7 @@ echo "Cloning Wake on LAN Server"
 
 rm -rf /var/www/html/wol-server/
 mkdir -p /var/www/html/wol-server
-chown -R $SUDO_USER:$SUDO_USER /var/www/html/wol-server
+chown -R www-data:www-data /var/www/html/wol-server
 
 git clone https://github.com/AndiSHFR/wake-on-lan.php.git
 
@@ -56,5 +60,17 @@ sleep 3
 
 cp -f ./wake-on-lan.php/wake-on-lan.php /var/www/html/wol-server/index.php
 rm -rf ./wake-on-lan.php
+
+cat <<EOF | tee /var/www/html/wol-server/config.json >/dev/null
+[
+    {
+        "mac": "$DEST_MAC",
+        "host": "$DEST_IP",
+        "cidr": "$DEST_SUBNET",
+        "port": "$DEST_PORT",
+        "comment": "Added via install script"
+    }
+]
+EOF
 
 echo "Installled Wake on LAN Server"
