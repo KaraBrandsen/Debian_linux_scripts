eb17ba5378a82e0658e344f139183f267a41266f
Kara Brandsen <karabrandsen@gmail.com>
2024-11-16 18:12:43 +0100

    Fixed Rust Desk Install

--- install/setup.sh
+++ install/setup.sh
@@ -1509,61 +1509,12 @@ fi
 #Rustdesk Client
 if [ "$INSTALL_RUSTDESK_CLIENT" == "true" ] ; then
     echo "-----------------------------Installing Rustdesk Client-----------------------------"
-
-    # Identify OS
-    if [ -f /etc/os-release ]; then
-        # freedesktop.org and systemd
-        . /etc/os-release
-        OS=$NAME
-        VER=$VERSION_ID
-
-        UPSTREAM_ID=${ID_LIKE,,}
-
-        # Fallback to ID_LIKE if ID was not 'ubuntu' or 'debian'
-        if [ "${UPSTREAM_ID}" != "debian" ] && [ "${UPSTREAM_ID}" != "ubuntu" ]; then
-            UPSTREAM_ID="$(echo ${ID_LIKE,,} | sed s/\"//g | cut -d' ' -f1)"
-        fi
-
-    elif type lsb_release >/dev/null 2>&1; then
-        # linuxbase.org
-        OS=$(lsb_release -si)
-        VER=$(lsb_release -sr)
-    elif [ -f /etc/lsb-release ]; then
-        # For some versions of Debian/Ubuntu without lsb_release command
-        . /etc/lsb-release
-        OS=$DISTRIB_ID
-        VER=$DISTRIB_RELEASE
-    elif [ -f /etc/debian_version ]; then
-        # Older Debian, Ubuntu, etc.
-        OS=Debian
-        VER=$(cat /etc/debian_version)
-    elif [ -f /etc/SuSE-release ]; then
-        # Older SuSE etc.
-        OS=SuSE
-        VER=$(cat /etc/SuSE-release)
-    elif [ -f /etc/redhat-release ]; then
-        # Older Red Hat, CentOS, etc.
-        OS=RedHat
-        VER=$(cat /etc/redhat-release)
-    else
-        # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
-        OS=$(uname -s)
-        VER=$(uname -r)
-    fi
-
-    # Install RustDesk
+    apt install xserver-xorg-video-all -y
 
     echo "Installing RustDesk"
-    if [ "${ID}" = "debian" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Debian" ] || [ "${UPSTREAM_ID}" = "ubuntu" ] || [ "${UPSTREAM_ID}" = "debian" ]; then
-        wget https://github.com/rustdesk/rustdesk/releases/download/1.2.6/rustdesk-1.3.2-x86_64.deb
-        apt-get install -fy ./rustdesk-1.3.2-x86_64.deb > null
-    elif [ "$OS" = "CentOS" ] || [ "$OS" = "RedHat" ] || [ "$OS" = "Fedora Linux" ] || [ "${UPSTREAM_ID}" = "rhel" ] || [ "$OS" = "Almalinux" ] || [ "$OS" = "Rocky*" ] ; then
-        wget https://github.com/rustdesk/rustdesk/releases/download/1.2.6/rustdesk-1.3.2-0.x86_64.rpm
-        yum localinstall ./rustdesk-1.3.2-0.x86_64.rpm -y > null
-    else
-        echo "Unsupported OS"
-        exit 1
-    fi
+
+    wget https://github.com/rustdesk/rustdesk/releases/download/1.3.2/rustdesk-1.3.2-x86_64.deb
+    apt-get install -fy ./rustdesk-1.3.2-x86_64.deb > null
 
     # Run the rustdesk command with --get-id and store the output in the rustdesk_id variable
     rustdesk_id=$(rustdesk --get-id)
@@ -1579,7 +1530,8 @@ if [ "$INSTALL_RUSTDESK_CLIENT" == "true" ] ; then
     else
         echo "Failed to get RustDesk ID."
     fi
-    
+
+    sed -i "s/#WaylandEnable=false/WaylandEnable=false/" "/etc/gdm3/custom.conf"
     echo "Installed Rustdesk Client"
 fi
 
