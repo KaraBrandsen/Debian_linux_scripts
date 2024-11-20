354cdbffdc3416271de9227c807899ee5b50ac9c
Kara Brandsen <karabrandsen@gmail.com>
2024-11-20 19:52:18 +0100

    Bug Fix

--- install/setup.sh
+++ install/setup.sh
@@ -118,7 +118,7 @@ if [ "$ARG" == "nas" ]; then
     DEFAULT_CONFIG=$(grep "GRUB_CMDLINE_LINUX_DEFAULT" /etc/default/grub)
     SDHCI_CONTROLLER=$(lspci | grep "SD Host")
 
-    if [[ $DEFAULT_CONFIG == *"sdhci.debug_quirks"* ]] || [ $SDHCI_CONTROLLER == "" ] || [ "$DEFAULT_CONFIG" == "" ]; then
+    if [[ $DEFAULT_CONFIG == *"sdhci.debug_quirks"* ]] || [ "$SDHCI_CONTROLLER" == "" ] || [ "$DEFAULT_CONFIG" == "" ]; then
         echo "No modification of Grub needed"
     else
         echo "Forcing NCQ disabled for eMMC devices"
