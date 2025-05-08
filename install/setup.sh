179d31ddc061881cc942dac1221139885cd8c963
Kara Brandsen <karabrandsen@gmail.com>
2025-05-08 22:53:57 +0200

    Added port config for convertx and stirling-pdf

--- install/setup.sh
+++ install/setup.sh
@@ -65,6 +65,12 @@ source "secrets.sh"
         RUSTDESK_PASS=$RUSTDESK_PASS                            #Rustdesk Client Password. Used to log into a device
         RUSTDESK_CFG=$RUSTDESK_CFG                              #Config string from and existing Rustdesk client. https://rustdesk.com/docs/en/self-host/client-configuration/#setup-using-import-or-export    
 
+    #ConvertX
+        CONVERTX_PORT=8100                                      #Port to be used for the ConvertX Web Interface
+
+    #Stirling PDF
+        STIRLING_PDF_PORT=8101                                  #Port to be used for the Stirling PDF Web Interface
+
     #Shell Extensions
         #Get the settings string by running the get_extension_settings.sh script on your current PC before reformatting.
         EXTENSION_SETTINGS='H4sIAAAAAAAAA+VWS2/jOAy++1cUuXgXqOIk7XSaAgb6mkOx7TToFgssiqBQJNrWRJYMic5jiv73pRwnTV+LorO3vSQ2yY8SyY+k7yT4KdoqqcCpqgDHtU9KW3sYR4ZjTe/MC2e1TjNSQRTdvYVAW4ui4vIdEM4ty5TJwbVyemZg+ESDTNHVz9w6NeNiOY4cVNYhQxCFUYJcVs4SoPQvEQ5Ki8A2r7IaRyvnK0vUnglwmMZJYUtIptzxpKst+Ux8wR0kuSE5e+2HEbQrHMaNjyksP+mCkPH7F54ZMY54jQUrAQsr07ji3s+tk/GzOJ4cePBeWTOOlNTBj+bLtFYG9wY7w16PDD0gUo49k5zOMkml61wZn1R2Dm4NU2VbHq8BKqYMF6hmwLhgqEqwNaZ7B+TuDfWygjQ2Fgs6JETmC9B6HEnlw3UZ0ccxWCCYcE3fHtNq5bbmLq4ntcGaSSumx6vnrrBlvC7hC2vuRAmmPm7/G9PdnVhyXzC0rOIG9PEPCc56GHZzhUU9WRvlXlhjQOAxN3JZWF2CX5soGyx+1B4Z8TojIzpwXaHjF/Jg+pdCCur4zDo4s5LS0N464zPrFFWXV1W4MEm7ubW5hu5Z4Ygj3dZpcGJd3m2I0/1O9Ve69m9rb8GVlH+9rcVZt9Kw6I7o53wl3qjH0Ry0CISUimubM80pAF/YuWEzcCGZabx/0O1tapc8ZTlpc0vlhIzXGll4YzMF8zQ+0fp+5GzueOnjqFFMakTKFcUL1BVGEDMuKNFxVDnIPOG8CpyoeA5pj7pagEG9JD55ymAocJspb3jFPFJC71tOeJvhnJprKy5PZ4iCZge6JZtQixDRHJeq9ulvoUl2dwZffo9CpA1vHCWNqjhTAjZj41W8z8hDnWhUyVcFVBQHKyyljK2kgRQNEtOH+OZiNLr8Fh/t7FM5Rpcn3/9on/+8uFop+o/R2kvJHXVgur8R0LQMtAmSGVe66RtqVUUJoGz0KPuWOGe9wqZap9e3t9dXcVRYpGniWbgUtT2jKk9sGt9+uxpd35zcXFz+HUcaMpzYxeYI1o+a2CgIUQT38UOn1znqXF2cn19+6zzGrRpoulJom1PXhncPnVZFoJDbE6rYaVP2zm6nLXDnqOny3c4aHWyRiynI28vO4+62j2aSkBH8kpcQ5qldbIMbCnwEi9xPJ9x9ChsIDO6DJ5/evEA7lRcfvfYrsF96hPKK2u5TcEm8/jx41YSvK/ZvHsYbcmkwORZrSvV7vY3Gq5+wlu8NgthRr1F/t+1AU4M8Yu3Zs8YhVqOjDth6nysjqfNp8IRpRVsMw+DZ9NDt9ejNgRfg40gUIKZs0e/P+VLThnh3XrzYBoSkD4HpakpuDuu9JWU2y2g1k/J/uQszRRvBLu7b//9kE45bErVbqCVDwylK86+swVkTFflfF0+ZVU5pZDcLZsKR5sByxZNGknGz/qQKrzOrMWy9lkjY3j3RkNMXLn3p2kxp8EfJ0aQvhRhKyb4MDgZsXx7SVjw82GPicPgVDuTw63AyfNrHITga/LouiQf9Qe+5wtm5pw+36B8kf6MK2wsAAA=='
@@ -341,6 +347,20 @@ if [ "$INSTALL_RADARR" == "true" ] ; then
 fi
 if [ "$INSTALL_PLEX_SERVER" == "true" ] ; then
     echo "Plex: $PLEX_URL/web"
+fi
+if [ "$INSTALL_CONVERTX" == "true" ] ; then
+    echo "ConvertX: http://$IP_LOCAL:$CONVERTX_PORT"
+
+    if [ "$INSTALL_ZEROTIER" == "true" ] ; then
+       echo "          http://$VIA_IP:$CONVERTX_PORT"
+    fi
+fi
+if [ "$INSTALL_STIRLING_PDF" == "true" ] ; then
+    echo "Stirling PDF: http://$IP_LOCAL:$STIRLING_PDF_PORT"
+
+    if [ "$INSTALL_ZEROTIER" == "true" ] ; then
+       echo "              http://$VIA_IP:$STIRLING_PDF_PORT"
+    fi
 fi
 echo ""
 if [ "$INSTALL_SHARES" == "true" ] ; then
