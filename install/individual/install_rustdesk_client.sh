e08d1f40a11618479141d869a3be850bfeba25e3
Kara Brandsen <karabrandsen@gmail.com>
2025-05-20 15:16:45 +0200

    Fixed rustdesk auto config

--- install/individual/install_rustdesk_client.sh
+++ install/individual/install_rustdesk_client.sh
@@ -8,18 +8,24 @@ fi
 
 #Variables
 RUSTDESK_PASS=$RUSTDESK_PASS                            #Rustdesk Client Password. Used to log into a device
-RUSTDESK_CFG=$RUSTDESK_CFG                              #Config string from and existing Rustdesk client. https://rustdesk.com/docs/en/self-host/client-configuration/#setup-using-import-or-export    
+RUSTDESK_SERVER=$RUSTDESK_SERVER                        #Rendezvous Server
+RUSTDESK_KEY=$RUSTDESK_KEY                              #Server Key
 
 
 echo "-----------------------------Installing Rustdesk Client-----------------------------"
 apt install xserver-xorg-video-all -y
 
-wget https://github.com/rustdesk/rustdesk/releases/download/1.3.2/rustdesk-1.3.2-x86_64.deb
-apt-get install -fy ./rustdesk-1.3.2-x86_64.deb > null
+wget https://github.com/rustdesk/rustdesk/releases/download/1.4.0/rustdesk-1.4.0-x86_64.deb
+apt-get install -fy ./rustdesk-1.4.0-x86_64.deb > null
 
 # Apply new password to RustDesk
 rustdesk --password $RUSTDESK_PASS &> /dev/null
-rustdesk --config $RUSTDESK_CFG
+rustdesk --option allow-remote-config-modification Y
+rustdesk --option custom-rendezvous-server $RUSTDESK_SERVER
+rustdesk --option key $RUSTDESK_KEY
+rustdesk --option enable-lan-discovery Y
+rustdesk --option direct-server Y
+
 systemctl restart rustdesk
 
 sed -i "s/#WaylandEnable=false/WaylandEnable=false/" "/etc/gdm3/custom.conf"
