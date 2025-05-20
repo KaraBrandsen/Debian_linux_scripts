c7ef1e74f9331a69ce63aeaf217fbbe222a443d2
Kara Brandsen <karabrandsen@gmail.com>
2025-05-20 13:32:30 +0200

    Fixed bugs with MOTD copy script

--- install/individual/install_motd.sh
+++ install/individual/install_motd.sh
@@ -2,21 +2,40 @@
 
 ARG=${1:-"desktop"}   
 
+(return 0 2>/dev/null) && SOURCED=1 || SOURCED=0
+
+if [ "$ARG" == "media" ]; then
+    ARG="desktop"
+fi
+
 echo "-----------------------------Installing MOTD Scripts-----------------------------"
 apt install lolcat figlet -y
 
 rm /etc/update-motd.d/*
 echo Copying MOTD files.
 
-if [ ! -f "../../motd/$ARG/10-hostname" ] ; then
-    if [ ! -f "./Debian_linux_scripts/motd/$ARG/10-hostname" ] ; then
-        git clone https://github.com/KaraBrandsen/Debian_linux_scripts.git
-    fi
+# if file is run standalone target dir is on level higher
+if [ ${SOURCED} -eq 0 ]; then
+    if [ ! -f "../../motd/$ARG/10-hostname" ] ; then
+        if [ ! -f "./Debian_linux_scripts/motd/$ARG/10-hostname" ] ; then
+            git clone https://github.com/KaraBrandsen/Debian_linux_scripts.git
+        fi
 
-    cp ./Debian_linux_scripts/motd/$ARG/* /etc/update-motd.d
+        cp ./Debian_linux_scripts/motd/$ARG/* /etc/update-motd.d
+    else
+        cp ../../motd/$ARG/* /etc/update-motd.d
+    fi
 else
-    cp ../../motd/$ARG/* /etc/update-motd.d
-fi
+    if [ ! -f "../motd/$ARG/10-hostname" ] ; then
+        if [ ! -f "./Debian_linux_scripts/motd/$ARG/10-hostname" ] ; then
+            git clone https://github.com/KaraBrandsen/Debian_linux_scripts.git
+        fi
+
+        cp ./Debian_linux_scripts/motd/$ARG/* /etc/update-motd.d
+    else
+        cp ../motd/$ARG/* /etc/update-motd.d
+    fi
+fi 
 
 /usr/bin/env figlet "$(hostname)" -w 100 | /usr/games/lolcat -f > /run/hostname_motd
 chmod +x -R /etc/update-motd.d/
